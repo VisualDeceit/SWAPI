0b19dcc8ad0b5560cb318baf45c68d920c1b7237
Alexander Fomin <inzhener@outlook.com>
2022-04-10 09:31:22 +0300

    Initial Commit

---
+++ SWAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWAPI
+//
+//  Created by Александр Фомин on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
